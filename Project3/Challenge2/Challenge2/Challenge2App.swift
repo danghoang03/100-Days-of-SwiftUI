9fec7ecb46daba9493bb87ad57f8dd1dae1eff49
Dang Hoang <danghmh1205@gmail.com>
2025-09-19 19:24:08 +0700

    Completed Project 3: Challenge 2

---
+++ Project3/Challenge2/Challenge2/Challenge2App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge2App.swift
+//  Challenge2
+//
+//  Created by Hoàng Minh Hải Đăng on 19/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Challenge2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
